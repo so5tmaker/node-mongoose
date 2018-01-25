88d09de39fe73534a7316ea3b1820296c5536758
Viktor Sivulskiy <svicvg@gmail.com>
2018-01-25 14:01:35 +0600

    Mongoose Part 3

--- index.js
+++ index.js
@@ -13,16 +13,14 @@ connect.then((db) => {
     console.log('Connected correctly to server');
 
     Dishes.create({
-        name: 'Uthapizza',
-        description: 'Test'
+        name: 'Uthappizza',
+        description: 'test'
     })
         .then((dish) => {
             console.log(dish);
 
             return Dishes.findByIdAndUpdate(dish._id, {
-                $set: {
-                    description: 'Updated Test'
-                }
+                $set: { description: 'Updated test' }
             }, {
                     new: true
                 })
@@ -30,13 +28,25 @@ connect.then((db) => {
         })
         .then((dish) => {
             console.log(dish);
+
+            dish.comments.push({
+                rating: 5,
+                comment: 'I\'m getting a sinking feeling!',
+                author: 'Leonardo di Carpaccio'
+            });
+
+            return dish.save();
+        })
+        .then((dish) => {
+            console.log(dish);
+
             return db.collection('dishes').drop();
         })
         .then(() => {
             return db.close();
         })
         .catch((err) => {
-
+            console.log(err);
         });
 
 });
